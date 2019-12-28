1cc846e9be7c8c21158d01f7bcd009b111a4e805
Kennyakins20 <46746360+Kennyakins20@users.noreply.github.com>
2019-12-28 02:13:21 -0500

    Create sysinfo.sh

---
+++ sysinfo.sh
@@ -0,0 +1,29 @@
+# bin/bash
+
+
+# This script script will return the following set of system information:
+echo -e "\e[31;43m***** HOSTNAME INFORMATION *****\e[0m"
+hostnamectl
+echo ""
+
+# File system disk space usage:
+echo -e "\e[31;43m***** FILE SYSTEM DISK SPACE USAGE *****\e[0m"
+df -h
+echo ""
+# Free and used memory in the system:
+echo -e "\e[31;43m***** FREE AND USED MEMORY *****\e[0m"
+free
+echo ""
+# System uptime and load:
+echo -e "\e[31;43m***** SYSTEM UPTIME AND LOAD *****\e[0m"
+uptime
+echo ""
+# Logged-in users:
+echo -e  "\e[31;43m***** CURRENTLY LOGGED-IN USERS *****\e[0m"
+who
+echo ""
+# Top 5 memory cosuming prcoesses
+echo -e "\e[31;43m***** TOP 5 MEMORY COSUMING PROCESSES *****\e[0m"
+ps -eo %mem,%cpu,comm --sort=-%mem | head -n 6
+ech0 ""
+echo -e "\e[1;32mDone.\e[0m"
